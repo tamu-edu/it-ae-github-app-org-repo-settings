33c15227befc5678560b6f03d6892a4a2495e386
SyahImran <ismatsyahimran@gmail.com>
2023-10-16 16:54:10 -0500

    Changed output of index.js to show contents of Github payload

--- github-repo-settings/index.js
+++ github-repo-settings/index.js
@@ -1,10 +1,12 @@
 module.exports.handler = async (event, context) => {
     console.log('Received event:', JSON.stringify(event, null, 2));
-    
     const obj = JSON.parse(event.body);
-    
-    console.log('value1 =', obj.key1);
-    console.log('value2 =', obj.key2);
-    console.log('value3 =', obj.key3);
-    return obj.key1;
+    console.log(obj)
+
+    const reponse = {
+        statusCode: 200,
+        body: JSON.stringify('Repository ' + obj.action)
+    };
+
+    return response;
 };
