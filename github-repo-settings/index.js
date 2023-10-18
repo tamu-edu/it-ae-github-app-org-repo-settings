417b2d6eaba85f02216b3bddcefacf49ce4e1b56
SyahImran <ismatsyahimran@gmail.com>
2023-10-18 13:20:18 -0500

    Reformatted output to only print for accepted events

--- github-repo-settings/index.js
+++ github-repo-settings/index.js
@@ -1,12 +1,21 @@
+import {App} from "octokit";
+
 module.exports.handler = async (event, context) => {
-    console.log('Received event:', JSON.stringify(event, null, 2));
     const obj = JSON.parse(event.body);
-    console.log(obj)
+    console.log('Received event: ' + obj);
 
+    const githubEvent = event.headers['x-gitHub-event'];
+
+    if (githubEvent === 'repository') {
+        const data = event.body;
+        const action = data.action;
+        if (action === 'created') {
+            console.log('A repository was created with this name: ' + data.name);
+        }
+    }
+    
     const response = {
-        statusCode: 200,
-        body: JSON.stringify('Repository ' + obj.action)
+        statusCode: 200
     };
-
     return response;
 };
