fe6ceea0588514310d2da6a23e0e4c4f2b029554
SyahImran <ismatsyahimran@gmail.com>
2023-10-20 16:11:23 -0500

    Configuring auth to Github through Octokit

--- github-repo-settings/index.mjs
+++ github-repo-settings/index.mjs
@@ -1,7 +1,10 @@
-import { App } from "octokit";
+import { Octokit } from "octokit";
+import { createAppAuth } from "@octokit/auth-app";
 
 export const handler = async (event, context) => {
   // console.log('Received event: ' + JSON.stringify(event, null, 2));
+  // const { Octokit } = require("@octokit/core");
+  // const { createAppAuth } = require("@octokit/auth-app");
 
   const githubEvent = event.headers["x-github-event"];
   if (githubEvent === "repository") {
@@ -18,15 +21,34 @@ export const handler = async (event, context) => {
       const webhookSecret = process.env.WEBHOOK_SECRET;
       const privateKey = process.env.PRIVATE_KEY;
 
-      const app = new App({
-        appId,
-        privateKey,
-        webhooks: { secret: webhookSecret },
+      // console.log("App ID: " + appId);
+      // console.log("Webhook Secret: " + webhookSecret);
+      // console.log("Private Key: " + privateKey);
+
+      const appOctokit = new Octokit({
+        authStrategy: createAppAuth,
+        auth: {
+          appId: appId,
+          privateKey: privateKey,
+          clientId: "Iv1.1389af67aba6167e",
+          clientSecret: "7d552329c5e4eeb37326c993c69a913622a43daf",
+          installationId: 123,
+        },
       });
 
       try {
         const requestString = `PUT /orgs/${data.organization.login}/teams/repo-settings-team/repos/${data.repository.full_name}}`;
-        await octokit.request(requestString, {
+        // await octokit.request(requestString, {
+        //   org: data.organization.login,
+        //   team_slug: "repo-settings-team",
+        //   owner: data.organization.login,
+        //   repo: data.repository.name,
+        //   permission: "admin",
+        //   headers: {
+        //     "X-GitHub-Api-Version": "2022-11-28",
+        //   },
+        // });
+        const { slug } = await appOctokit.request(requestString, {
           org: data.organization.login,
           team_slug: "repo-settings-team",
           owner: data.organization.login,
