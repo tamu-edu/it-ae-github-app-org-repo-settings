5881732048b35c83e2fb1f7550e0cac0d6bb4cd3
SyahImran <ismatsyahimran@gmail.com>
2023-10-26 10:59:07 -0500

    Changed installationId to match the installed github app

--- github-repo-settings/index.mjs
+++ github-repo-settings/index.mjs
@@ -17,56 +17,92 @@ export const handler = async (event, context) => {
         "A repository was created with this name: " + data.repository.name
       );
 
-      const appId = process.env.APP_ID;
-      const webhookSecret = process.env.WEBHOOK_SECRET;
+      // const appId = process.env.APP_ID;
+      // const webhookSecret = process.env.WEBHOOK_SECRET;
       // const privateKey = process.env.PRIVATE_KEY;
 
       // Retrieve the private key from AWS Secrets Manager
-      const secret_name = "test/github-repo-settings/PRIVATE_KEY";
-
       const client = new SecretsManagerClient({
         region: "us-east-2",
       });
-
       let response;
+      let privateKey;
+      let appId;
+      let webhookSecret;
+      let clientId;
+      let clientSecret;
+      // const secret_name = "test/github-repo-settings/PRIVATE_KEY";
 
       try {
         response = await client.send(
           new GetSecretValueCommand({
-            SecretId: secret_name,
+            SecretId: "test/github-repo-settings/PRIVATE_KEY",
             VersionStage: "AWSCURRENT",
           })
         );
+        privateKey = response.SecretString;
+        response = await client.send(
+          new GetSecretValueCommand({
+            SecretId: "test/github-repo-settings/APP_ID",
+            VersionStage: "AWSCURRENT",
+          })
+        );
+        appId = response.SecretString;
+        response = await client.send(
+          new GetSecretValueCommand({
+            SecretId: "test/github-repo-settings/WEBHOOK_SECRET",
+            VersionStage: "AWSCURRENT",
+          })
+        );
+        webhookSecret = response.SecretString;
+        response = await client.send(
+          new GetSecretValueCommand({
+            SecretId: "test/github-repo-settings/CLIENT_ID",
+            VersionStage: "AWSCURRENT",
+          })
+        );
+        clientId = response.SecretString;
+        response = await client.send(
+          new GetSecretValueCommand({
+            SecretId: "test/github-repo-settings/CLIENT_SECRET",
+            VersionStage: "AWSCURRENT",
+          })
+        );
+        clientSecret = response.SecretString;
       } catch (error) {
         throw error;
       }
+      // const privateKey = response.SecretString;
 
-      const secret = response.SecretString;
+      // secret_name = "test/github-repo-settings/PRIVATE_KEY";
+      // try {
+      //   response = await client.send(
+      //     new GetSecretValueCommand({
+      //       SecretId: secret_name,
+      //       VersionStage: "AWSCURRENT",
+      //     })
+      //   );
+      // } catch (error) {
+      //   throw error;
+      // }
+      // const appId = response.SecretString;
       /////////////////////////////////////////////////
 
       const appOctokit = new Octokit({
         authStrategy: createAppAuth,
         auth: {
           appId: appId,
-          privateKey: secret,
-          clientId: "Iv1.1389af67aba6167e",
-          clientSecret: "7d552329c5e4eeb37326c993c69a913622a43daf",
-          installationId: 123,
+          privateKey: privateKey,
+          clientId: clientId,
+          clientSecret: clientSecret,
+          installationId: 43330184,
         },
       });
 
       try {
         const requestString = `PUT /orgs/${data.organization.login}/teams/repo-settings-team/repos/${data.repository.full_name}}`;
-        // await octokit.request(requestString, {
-        //   org: data.organization.login,
-        //   team_slug: "repo-settings-team",
-        //   owner: data.organization.login,
-        //   repo: data.repository.name,
-        //   permission: "admin",
-        //   headers: {
-        //     "X-GitHub-Api-Version": "2022-11-28",
-        //   },
-        // });
+        // const { slug } = await appOctokit.request("GET /user");
+        // console.log("authenticated as %s", slug);
         const { slug } = await appOctokit.request(requestString, {
           org: data.organization.login,
           team_slug: "repo-settings-team",
@@ -84,6 +120,7 @@ export const handler = async (event, context) => {
           );
         }
         console.error(error);
+        throw error;
       }
     }
   } else {
