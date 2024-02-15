bc34babfa4a78179c14e444baabbe641087c8ee0
SyahImran <ismatsyahimran@gmail.com>
2024-02-15 13:50:34 -0600

    Added function to validate webhooks sent by Github

--- gh-app-org-repo-settings/index.mjs
+++ gh-app-org-repo-settings/index.mjs
@@ -41,6 +41,19 @@ export const handler = async (event) => {
         const webhookSecret = response.SecretString;
         /////////////////////////////////////////////////
 
+        // Verify the signature
+        const signature = event.headers["x-hub-signature-256"];
+        const payload = event.body;
+        const secret = webhookSecret;
+        const header = "sha256=" + signature;
+        const verified = await verifySignature(secret, header, payload);
+        if (!verified) {
+          return {
+            statusCode: 401,
+          };
+        }
+        ////////////////////////////////////////////////
+
         // Authenticate with Github
         const appOctokit = new App({
           appId: appId,
@@ -142,3 +155,48 @@ const updateTeamPermissions = async (octokit, org, repo, grantAccessTeams) => {
     });
   }
 };
+
+let encoder = new TextEncoder();
+
+async function verifySignature(secret, header, payload) {
+  let parts = header.split("=");
+  let sigHex = parts[1];
+
+  let algorithm = { name: "HMAC", hash: { name: "SHA-256" } };
+
+  let keyBytes = encoder.encode(secret);
+  let extractable = false;
+  let key = await crypto.subtle.importKey(
+    "raw",
+    keyBytes,
+    algorithm,
+    extractable,
+    ["sign", "verify"]
+  );
+
+  let sigBytes = hexToBytes(sigHex);
+  let dataBytes = encoder.encode(payload);
+  let equal = await crypto.subtle.verify(
+    algorithm.name,
+    key,
+    sigBytes,
+    dataBytes
+  );
+
+  return equal;
+}
+
+function hexToBytes(hex) {
+  let len = hex.length / 2;
+  let bytes = new Uint8Array(len);
+
+  let index = 0;
+  for (let i = 0; i < hex.length; i += 2) {
+    let c = hex.slice(i, i + 2);
+    let b = parseInt(c, 16);
+    bytes[index] = b;
+    index += 1;
+  }
+
+  return bytes;
+}
