5c82297fc890577d4e6303bfa2e9dffd376c0f19
Joseph Rafferty <jrafferty@tamu.edu>
2024-03-18 09:25:51 -0500

    Override dynamodb name

--- terraform-state/main.tf
+++ terraform-state/main.tf
@@ -1,6 +1,7 @@
 module "state_backend" {
 	source = "github.com/tamu-edu/it-ae-tfmod-aws-state?ref=v0.0.2"
 	bucket_name = "gh-app-org-repo-settings"
+	dynamodb_table_name = "gh-app-org-repo-settings"
 }
 
 output "region" {
